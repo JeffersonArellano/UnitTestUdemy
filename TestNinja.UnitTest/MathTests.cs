c55ee1437f6c329ddb99636f207929617b135ff4
Jefferson Arellano <Jefferson.Arellano@travelport.com>
2019-10-16 14:42:06 +0200

    initial  load

---
+++ TestNinja.UnitTest/MathTests.cs
@@ -0,0 +1,45 @@
+﻿namespace TestNinja.UnitTest
+{
+    using NUnit.Framework;
+    using TestNinja.Fundamentals;
+
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void Setup()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+       // [Ignore("Becase I want do!!")]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            //Arrange
+            var a = 1;
+            var b = 2;
+
+            //Act
+            var result = _math.Add(a, b);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            //Act
+            var result = _math.Max(a, b);
+
+            //Assert  
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ TestNinja.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec54c1b5-17f3-4c23-b6ca-26fc6b321fd0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
